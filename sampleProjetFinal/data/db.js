6663f16de6ceb5ac1be674436723901f18232e5a
Ricardo Rouco <ricardo.rouco@gmail.com>
2022-10-13 15:04:36 +0100

    Update db.js

--- sampleProjetFinal/data/db.js
+++ sampleProjetFinal/data/db.js
@@ -5,22 +5,22 @@ const perfis = [
 
 const usuarios = [{
     id: 1,
-    nome: 'João Silva',
-    email: 'jsilva@zemail.com',
+    nome: 'Ricardo',
+    email: 'rdsr@zemail.com',
     idade: 29,
     perfil_id: 1,
     status: 'ATIVO'
 }, {
     id: 2,
-    nome: 'Rafael Junior',
-    email: 'rafajun@wemail.com',
+    nome: 'paulo Junior',
+    email: 'pauj@wemail.com',
     idade: 31,
     perfil_id: 2,
     status: 'INATIVO'
 }, {
     id: 3,
-    nome: 'Daniela Smith',
-    email: 'danismi@umail.com',
+    nome: 'Patricia Smith',
+    email: 'patsmi@umail.com',
     idade: 24,
     perfil_id: 1,
     status: 'BLOQUEADO'
